a01b116b1ccdaf245f2e6ae89a25cd3de36f41a6
loick <loick.ledigabel@gmail.com>
2016-02-29 14:45:06 +0100

    init

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 "use strict";
 
 var loaderUtils = require("loader-utils");
-var fs = require('fs');
-var path = require("path");
-module.exports = function(content) {
-  var query = loaderUtils.parseQuery(this.query).path;
-  var queryString = JSON.stringify(query);
-  var varPath = queryString.replace(/["']/g, '');
-  this.cacheable();
-  var contentPath = path.resolve(varPath);
-  this.addDependency(contentPath);
-  var obj = JSON.parse(fs.readFileSync(contentPath, 'utf8'));
+// var fs = require('fs');
+// var path = require("path");
 
+module.exports = function(content)
+{
+  var obj = loaderUtils.parseQuery(this.query).vars;
+  obj = JSON.parse(obj);
+
+  this.cacheable();
 
   function jsonToSassVars (obj, indent) {
     // Make object root properties into sass variables
@@ -21,25 +19,24 @@ module.exports = function(content) {
     }
 
     // Store string values (so they remain unaffected)
-    var storedStrings = [];
-    sass = sass.replace(/(["'])(?:(?=(\\?))\2.)*?\1/g, function (str) {
+    // var storedStrings = [];
+    // sass = sass.replace(/(["'])(?:(?=(\\?))\2.)*?\1/g, function (str) {
 
-      var id = "___JTS" + storedStrings.length;
-      storedStrings.push({id: id, value: str});
-      return id;
-    });
+    //   var id = "___JTS" + storedStrings.length;
+    //   storedStrings.push({id: id, value: str});
+    //   return id;
+    // });
 
     // Convert js lists and objects into sass lists and maps
     sass = sass.replace(/[{\[]/g, "(").replace(/[}\]]/g, ")");
 
     // Put string values back (now that we're done converting)
-    storedStrings.forEach(function (str) {
-      sass = sass.replace(str.id, str.value);
-    });
+    // storedStrings.forEach(function (str) {
+    //   sass = sass.replace(str.id, str.value);
+    // });
 
     return sass;
   }
-
 
   var sass = jsonToSassVars(obj);
 
